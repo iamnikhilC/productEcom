a11989ff5e12a1335226aba601ff5b356c96684e
iamnikhil <nikhilchichghare000@gmail.com>
2024-10-12 17:43:08 +0530

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{route('dashboard')}}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
@@ -73,9 +73,6 @@
         </nav>
 
         <main class="py-4">
-            @if(Auth::user())
-            @include('layouts.sideBar')
-            @endif
             @yield('content')
         </main>
     </div>
